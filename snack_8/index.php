4315b1eeb04da7891e40af0fd4c46b9001b79c35
husqy06 <luigf306@gmail.com>
2021-10-14 21:24:53 +0200

    Completamento snack_8

--- snack_8/index.php
+++ snack_8/index.php
@@ -1,36 +1,20 @@
 <!-- 
     Partendo da questo array: https://www.codepile.net/pile/Po60bjgQ
-    Ad ogni refresh della pagina visualizzare una pubblicità a schermo, tenendo conto che possono essere sorteggiate solo quelle is_active true.
+    Ad ogni refresh della pagina visualizzare una pubblicità a schermo, 
+    tenendo conto che possono essere sorteggiate solo quelle is_active true.
 -->
 <?php
-    $ads = [
-        [
-            'image_path' => 'https://conversionsciences.com/wp-content/uploads/2019/04/example-of-emotional-logical-appeal-on-persuasive-copy.jpg',
-            'link' => 'https://www.google.com',
-            'is_active' => true,
-        ],
-        [
-            'image_path' => 'https://previews.123rf.com/images/hstrongart/hstrongart1612/hstrongart161200121/68060446-anuncios-de-cepillos-de-dientes-el%C3%A9ctricos-modo-diferente-de-este-producto-con-blanco-modelo-de-diente.jpg',
-            'link' => 'https://www.facebook.com',
-            'is_active' => true,
-        ],
-        [
-            'image_path' => 'https://thumbs.dreamstime.com/z/back-to-school-sale-background-chalkboard-sale-percentages-marketing-poster-color-pencils-96751666.jpg',
-            'link' => 'https://codepen.io',
-            'is_active' => false,
-        ],
-        [
-            'image_path' => 'https://foodsecurityfoodjustice.files.wordpress.com/2016/11/beyond-the-omlette-1.jpg',
-            'link' => 'https://laravel.com',
-            'is_active' => false,
-        ],
-        [
-            'image_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRniLfGdFLxVOUoEwYC9WeRIrfZInB74_80IV8yMnANx3HTZYoQ',
-            'link' => 'https://www.php.net',
-            'is_active' => true,
-        ],
-    
-    ];
+    include "database.php";
+   
+    $active_ads = [];
+
+    foreach($ads as $ad) {
+        if($ad["is_active"]) {
+            $active_ads[] = $ad;
+        }
+    }
+
+    $current_ad = $active_ads[rand(0, count($active_ads) -1)];
 ?>
 
 <!DOCTYPE html>
@@ -40,17 +24,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snack_8</title>
+
+    <style>
+        .container {
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+        img {
+            width: 30%;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
-        <h1>ciao</h1>
-        <img src="
-            <?php
-                
-                echo $ads[rand(0, count($ads))]["image_path"];
-                
-            ?>
-        " alt="">
+        <img src="<?php echo $current_ad["image_path"]; ?>" alt="">
+        <h2><a href="<?php echo $current_ad["link"]?>"><?php echo $current_ad["link"]?></a></h2>
     </div>
 </body>
 </html>
